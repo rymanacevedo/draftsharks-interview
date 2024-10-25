a29975b3cabb8a0c573e5fd636e4255e719bcd80
Ryan Acevedo <ryan.avocado@protonmail.com>
2024-10-25 22:30:58 +0000

    added sub rosters and ability to track based off selected team

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-import Card from './components/Card.js'
-import Grid from './components/Grid.js'
-import Navigation from './components/Navigation.js'
-const { createApp } = Vue
+import Card from './components/Card.js';
+import Grid from './components/Grid.js';
+import Navigation from './components/Navigation.js';
+const { createApp } = Vue;
 
 createApp({
   components: {
@@ -17,7 +17,34 @@ createApp({
         { name: 'Baltimore Ravens', value: '3' },
         { name: 'Buffalo Bills', value: '4' },
         { name: 'Carolina Panthers', value: '5' },
-        { name: 'Chicago Bears', value: '6' }
+        { name: 'Chicago Bears', value: '6' },
+        { name: 'Cincinnati Bengals', value: '7' },
+        { name: 'Cleveland Browns', value: '8' },
+        { name: 'Dallas Cowboys', value: '9' },
+        { name: 'Denver Broncos', value: '10' },
+        { name: 'Detroit Lions', value: '11' },
+        { name: 'Green Bay Packers', value: '12' },
+        { name: 'Houston Texans', value: '13' },
+        { name: 'Indianapolis Colts', value: '14' },
+        { name: 'Jacksonville Jaguars', value: '15' },
+        { name: 'Kansas City Chiefs', value: '16' },
+        { name: 'Las Vegas Raiders', value: '23' },
+        { name: 'Los Angeles Chargers', value: '26' },
+        { name: 'Los Angeles Rams', value: '29' },
+        { name: 'Miami Dolphins', value: '17' },
+        { name: 'Minnesota Vikings', value: '18' },
+        { name: 'New England Patriots', value: '19' },
+        { name: 'New Orleans Saints', value: '20' },
+        { name: 'New York Giants', value: '21' },
+        { name: 'New York Jets', value: '22' },
+        { name: 'Philadelphia Eagles', value: '24' },
+        { name: 'Pittsburgh Steelers', value: '25' },
+        { name: 'Rookie', value: '34' },
+        { name: 'San Francisco 49ers', value: '28' },
+        { name: 'Seattle Seahawks', value: '27' },
+        { name: 'Tampa Bay Buccaneers', value: '30' },
+        { name: 'Tennessee Titans', value: '31' },
+        { name: 'Washington Commanders', value: '32' }
       ],
       selectedOption: '1',
       rosters: {
@@ -86,17 +113,303 @@ createApp({
             "SWR": ["Velus Jones Jr."],
             "TE": ["Cole Kmet", "Robert Tonyan"]
           }
-        }
+        },
+          "7": {
+            "team": "Cincinnati Bengals",
+            "positions": {
+              "QB": ["Joe Burrow"],
+              "RB": ["Joe Mixon", "Trayveon Williams"],
+              "LWR": ["Ja'Marr Chase"],
+              "RWR": ["Tee Higgins"],
+              "SWR": ["Tyler Boyd"],
+              "TE": ["Irv Smith Jr."]
+            }
+          },
+          "8": {
+            "team": "Cleveland Browns",
+            "positions": {
+              "QB": ["Deshaun Watson"],
+              "RB": ["Nick Chubb", "Jerome Ford"],
+              "LWR": ["Amari Cooper"],
+              "RWR": ["Donovan Peoples-Jones"],
+              "SWR": ["Elijah Moore"],
+              "TE": ["David Njoku"]
+            }
+          },
+          "9": {
+            "team": "Dallas Cowboys",
+            "positions": {
+              "QB": ["Dak Prescott"],
+              "RB": ["Tony Pollard", "Malik Davis"],
+              "LWR": ["CeeDee Lamb"],
+              "RWR": ["Michael Gallup"],
+              "SWR": ["Brandin Cooks"],
+              "TE": ["Jake Ferguson"]
+            }
+          },
+          "10": {
+            "team": "Denver Broncos",
+            "positions": {
+              "QB": ["Russell Wilson"],
+              "RB": ["Javonte Williams", "Samaje Perine"],
+              "LWR": ["Jerry Jeudy"],
+              "RWR": ["Courtland Sutton"],
+              "SWR": ["KJ Hamler"],
+              "TE": ["Greg Dulcich"]
+            }
+          },
+          "11": {
+            "team": "Detroit Lions",
+            "positions": {
+              "QB": ["Jared Goff"],
+              "RB": ["David Montgomery", "Jahmyr Gibbs"],
+              "LWR": ["Amon-Ra St. Brown"],
+              "RWR": ["Jameson Williams"],
+              "SWR": ["Kalif Raymond"],
+              "TE": ["Sam LaPorta"]
+            }
+          },
+          "12": {
+            "team": "Green Bay Packers",
+            "positions": {
+              "QB": ["Jordan Love"],
+              "RB": ["Aaron Jones", "AJ Dillon"],
+              "LWR": ["Christian Watson"],
+              "RWR": ["Romeo Doubs"],
+              "SWR": ["Jayden Reed"],
+              "TE": ["Luke Musgrave"]
+            }
+          },
+          "13": {
+            "team": "Houston Texans",
+            "positions": {
+              "QB": ["C.J. Stroud"],
+              "RB": ["Dameon Pierce", "Devin Singletary"],
+              "LWR": ["Nico Collins"],
+              "RWR": ["Robert Woods"],
+              "SWR": ["Tank Dell"],
+              "TE": ["Dalton Schultz"]
+            }
+          },
+          "14": {
+            "team": "Indianapolis Colts",
+            "positions": {
+              "QB": ["Anthony Richardson"],
+              "RB": ["Jonathan Taylor", "Zack Moss"],
+              "LWR": ["Michael Pittman Jr."],
+              "RWR": ["Alec Pierce"],
+              "SWR": ["Josh Downs"],
+              "TE": ["Mo Alie-Cox"]
+            }
+          },
+          "15": {
+            "team": "Jacksonville Jaguars",
+            "positions": {
+              "QB": ["Trevor Lawrence"],
+              "RB": ["Travis Etienne", "Tank Bigsby"],
+              "LWR": ["Calvin Ridley"],
+              "RWR": ["Zay Jones"],
+              "SWR": ["Christian Kirk"],
+              "TE": ["Evan Engram"]
+            }
+          },
+          "16": {
+            "team": "Kansas City Chiefs",
+            "positions": {
+              "QB": ["Patrick Mahomes"],
+              "RB": ["Isiah Pacheco", "Jerick McKinnon"],
+              "LWR": ["Marquez Valdes-Scantling"],
+              "RWR": ["Skyy Moore"],
+              "SWR": ["Kadarius Toney"],
+              "TE": ["Travis Kelce"]
+            }
+          },
+          "17": {
+            "team": "Miami Dolphins",
+            "positions": {
+              "QB": ["Tua Tagovailoa"],
+              "RB": ["Raheem Mostert", "Devon Achane"],
+              "LWR": ["Tyreek Hill"],
+              "RWR": ["Jaylen Waddle"],
+              "SWR": ["Braxton Berrios"],
+              "TE": ["Durham Smythe"]
+            }
+          },
+          "18": {
+            "team": "Minnesota Vikings",
+            "positions": {
+              "QB": ["Kirk Cousins"],
+              "RB": ["Alexander Mattison", "Ty Chandler"],
+              "LWR": ["Justin Jefferson"],
+              "RWR": ["K.J. Osborn"],
+              "SWR": ["Jordan Addison"],
+              "TE": ["T.J. Hockenson"]
+            }
+          },
+          "19": {
+            "team": "New England Patriots",
+            "positions": {
+              "QB": ["Mac Jones"],
+              "RB": ["Rhamondre Stevenson", "Ezekiel Elliott"],
+              "LWR": ["DeVante Parker"],
+              "RWR": ["JuJu Smith-Schuster"],
+              "SWR": ["Kendrick Bourne"],
+              "TE": ["Hunter Henry", "Mike Gesicki"]
+            }
+          },
+          "20": {
+            "team": "New Orleans Saints",
+            "positions": {
+              "QB": ["Derek Carr"],
+              "RB": ["Alvin Kamara", "Jamaal Williams"],
+              "LWR": ["Chris Olave"],
+              "RWR": ["Michael Thomas"],
+              "SWR": ["Rashid Shaheed"],
+              "TE": ["Juwan Johnson"]
+            }
+          },
+          "21": {
+            "team": "New York Giants",
+            "positions": {
+              "QB": ["Daniel Jones"],
+              "RB": ["Saquon Barkley", "Matt Breida"],
+              "LWR": ["Darius Slayton"],
+              "RWR": ["Isaiah Hodgins"],
+              "SWR": ["Wan'Dale Robinson"],
+              "TE": ["Darren Waller"]
+            }
+          },
+          "22": {
+            "team": "New York Jets",
+            "positions": {
+              "QB": ["Aaron Rodgers"],
+              "RB": ["Breece Hall", "Dalvin Cook"],
+              "LWR": ["Garrett Wilson"],
+              "RWR": ["Allen Lazard"],
+              "SWR": ["Mecole Hardman"],
+              "TE": ["Tyler Conklin"]
+            }
+          },
+          "23": {
+            "team": "Las Vegas Raiders",
+            "positions": {
+              "QB": ["Jimmy Garoppolo"],
+              "RB": ["Josh Jacobs", "Ameer Abdullah"],
+              "LWR": ["Davante Adams"],
+              "RWR": ["Hunter Renfrow"],
+              "SWR": ["Jakobi Meyers"],
+              "TE": ["Austin Hooper"]
+            }
+          },
+          "24": {
+            "team": "Philadelphia Eagles",
+            "positions": {
+              "QB": ["Jalen Hurts"],
+              "RB": ["D'Andre Swift", "Kenneth Gainwell"],
+              "LWR": ["A.J. Brown"],
+              "RWR": ["DeVonta Smith"],
+              "SWR": ["Quez Watkins"],
+              "TE": ["Dallas Goedert"]
+            }
+          },
+          "25": {
+            "team": "Pittsburgh Steelers",
+            "positions": {
+              "QB": ["Kenny Pickett"],
+              "RB": ["Najee Harris", "Jaylen Warren"],
+              "LWR": ["Diontae Johnson"],
+              "RWR": ["George Pickens"],
+              "SWR": ["Calvin Austin III"],
+              "TE": ["Pat Freiermuth"]
+            }
+          },
+          "26": {
+            "team": "Los Angeles Chargers",
+            "positions": {
+              "QB": ["Justin Herbert"],
+              "RB": ["Austin Ekeler", "Joshua Kelley"],
+              "LWR": ["Keenan Allen"],
+              "RWR": ["Mike Williams"],
+              "SWR": ["Quentin Johnston"],
+              "TE": ["Gerald Everett"]
+            }
+          },
+          "27": {
+            "team": "Seattle Seahawks",
+            "positions": {
+              "QB": ["Geno Smith"],
+              "RB": ["Kenneth Walker III", "Zach Charbonnet"],
+              "LWR": ["DK Metcalf"],
+              "RWR": ["Tyler Lockett"],
+              "SWR": ["Jaxon Smith-Njigba"],
+              "TE": ["Noah Fant"]
+            }
+          },
+          "28": {
+            "team": "San Francisco 49ers",
+            "positions": {
+              "QB": ["Brock Purdy"],
+              "RB": ["Christian McCaffrey", "Elijah Mitchell"],
+              "LWR": ["Deebo Samuel"],
+              "RWR": ["Brandon Aiyuk"],
+              "SWR": ["Jauan Jennings"],
+              "TE": ["George Kittle"]
+            }
+          },
+          "29": {
+            "team": "Los Angeles Rams",
+            "positions": {
+              "QB": ["Matthew Stafford"],
+              "RB": ["Cam Akers", "Kyren Williams"],
+              "LWR": ["Cooper Kupp"],
+              "RWR": ["Van Jefferson"],
+              "SWR": ["Puka Nacua"],
+              "TE": ["Tyler Higbee"]
+            }
+          },
+          "30": {
+            "team": "Tampa Bay Buccaneers",
+            "positions": {
+              "QB": ["Baker Mayfield"],
+              "RB": ["Rachaad White", "Chase Edmonds"],
+              "LWR": ["Mike Evans"],
+              "RWR": ["Chris Godwin"],
+              "SWR": ["Russell Gage"],
+              "TE": ["Cade Otton"]
+            }
+          },
+          "31": {
+            "team": "Tennessee Titans",
+            "positions": {
+              "QB": ["Ryan Tannehill"],
+              "RB": ["Derrick Henry", "Tyjae Spears"],
+              "LWR": ["Treylon Burks"],
+              "RWR": ["Nick Westbrook-Ikhine"],
+              "SWR": ["Kyle Philips"],
+              "TE": ["Chigoziem Okonkwo"]
+            }
+          },
+          "32": {
+            "team": "Washington Commanders",
+            "positions": {
+              "QB": ["Sam Howell"],
+              "RB": ["Brian Robinson Jr.", "Antonio Gibson"],
+              "LWR": ["Terry McLaurin"],
+              "RWR": ["Curtis Samuel"],
+              "SWR": ["Jahan Dotson"],
+              "TE": ["Logan Thomas"]
+            }
+          }
       }
-    }
+    };
   },
   computed: {
     selectedTeamName() {
-      const team = this.teams.find(team => team.value === this.selectedOption)
-      return team ? team.name : ''
+      const team = this.teams.find(team => team.value === this.selectedOption);
+      return team ? team.name : '';
     },
     selectedTeamPositions() {
-      return this.rosters[this.selectedOption]?.positions || {}
+      return this.rosters[this.selectedOption]?.positions || {};
     }
   },
   template: `
@@ -114,7 +427,7 @@ createApp({
     </card>
 
     <!-- Offense Card with Dynamic Player List -->
-    <card heading="Offense">
+    <card :heading="'Offense ' + selectedTeamName">
       <h3>Quarterbacks (QB):</h3>
       <ul>
         <li v-for="player in selectedTeamPositions.QB" :key="player">{{ player }}</li>
@@ -123,11 +436,8 @@ createApp({
       <ul>
         <li v-for="player in selectedTeamPositions.RB" :key="player">{{ player }}</li>
       </ul>
-      <!-- Add other positions as needed -->
     </card>
-
-    <!-- Defense & Special Teams Card -->
-    <card heading="Defense & Special Teams">
+    <card :heading="selectedTeamName  +' Defense & Special Teams'">
       <h3>Tight Ends (TE):</h3>
       <ul>
         <li v-for="player in selectedTeamPositions.TE" :key="player">{{ player }}</li>
@@ -135,4 +445,4 @@ createApp({
       <!-- Add other defensive positions or special teams as needed -->
     </card>
   `,
-}).mount('#app')
+}).mount('#app');
