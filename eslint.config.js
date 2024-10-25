8385874284d4f0f9e9ca38de94c781fe5064afae
Ryan Acevedo <ryan.avocado@protonmail.com>
2024-10-25 19:47:30 +0000

    modified eslint to modern eslint config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.js
+import vue from 'eslint-plugin-vue'
+import prettier from 'eslint-plugin-prettier'
+
+export default [
+  {
+    files: ['**/*.js', '**/*.vue'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: {
+      vue,
+      prettier,
+    },
+    rules: {
+      ...vue.configs.essential.rules, // Use Vue's essential rules
+      'prettier/prettier': 'error', // Prettier formatting issues show as errors
+      'vue/no-unused-vars': 'warn', // Warn about unused vars in templates
+    },
+  },
+]
