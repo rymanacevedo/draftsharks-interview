31e9b50b84e4b9c1bbd7249ac4c1c932b15b9b5c
Ryan Acevedo <ryan.avocado@protonmail.com>
2024-10-30 21:12:53 +0000

    adjusted card styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,13 +14,13 @@ export default {
     },
   },
   template: `
-      <div class="card">
-        <h2 class="card-heading">{{ heading }}</h2>
+      <div class="card overflow-hidden shadow-3 mr3 ml3 pb3 ba br3 bw0">
+        <h2 class="card-heading mt0 pa2 pt3 pb3 bg-light-gray">{{ heading }}</h2>
         <div class="flex flex-row items-center justify-center mb3">
           <img class="mr3" v-if="showImage" src="https://placehold.co/120x100"/>
           <h3 v-if="selectedHeader">{{ selectedHeader }}</h3>
         </div>
-        <div class="card-content">
+        <div class="card-content pa2">
           <slot></slot> <!-- Slot for additional content, like the select box -->
         </div>
       </div>
