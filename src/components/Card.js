55757e842b652ee03f9c6a9e7d80054990cce66b
Ryan Acevedo <ryan.avocado@protonmail.com>
2024-10-30 02:36:52 +0000

    updated card with iamge

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,29 @@
 export default {
-    props: {
-      heading: {
-        type: String,
-        required: true
-      },
-      selectedHeader: {
-        type: String,
-        default: null
-      }
+  props: {
+    heading: {
+      type: String,
+      required: true,
     },
-    template: `
+    selectedHeader: {
+      type: String,
+      default: null,
+    },
+    showImage: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  template: `
       <div class="card">
         <h2 class="card-heading">{{ heading }}</h2>
-        <h3 v-if="selectedHeader">{{ selectedHeader }}</h3> <!-- Render selected team name here -->
-        <div class="card-content">
+        <div class="flex flex-row items-center justify-center mb3">
+          <img class="mr3" v-if="showImage" src="https://placehold.co/120x100"/>
+          <h3 v-if="selectedHeader">{{ selectedHeader }}</h3>
+        </div>
+      </div>
+      <div class="card-content">
           <slot></slot> <!-- Slot for additional content, like the select box -->
         </div>
       </div>
-    `
-  }
-  
+    `,
+};
