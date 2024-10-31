2d87cb477576e4ce77e7ad54f945483c645adb84
Ryan Acevedo <ryan.avocado@protonmail.com>
2024-10-31 17:47:14 +0000

    fix missing tag

--- src/components/PlayerTable.js
+++ src/components/PlayerTable.js
@@ -11,7 +11,6 @@ export default {
   },
   template: `
       <div class="flex justify-center pa4">
-        <div class="overflow-auto">
           <table class="custom-table f6 w-100 mw8 center">
             <thead>
               <tr><th>Jersey Number</th><th>Player Name</th></tr>
@@ -22,9 +21,8 @@ export default {
                 <td class="pa3">{{ player.name }}</td>
               </tr>
             </tbody>
-            <caption class="mt2">Player Roster</caption>
+            <caption class="mt4">Player Roster</caption>
           </table>
-        </div>
       </div>
     `,
 };
